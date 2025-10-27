3ab0b9433fdf4ecba8ab8e56e56808a469e023ff
Emshtml <casadathereza1@gmail.com>
2025-10-27 13:13:05 -0300

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-zinc-950 text-zinc-400 py-4 text-center mt-10 border-t border-zinc-800">
+      <p>© {new Date().getFullYear()} 🍔 Burger House — Feito com amor e muito sabor ❤️</p>
+    </footer>
+  );
+}
