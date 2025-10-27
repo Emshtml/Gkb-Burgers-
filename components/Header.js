8361daf3829299b9c0ddcef98b26d816c1d44934
Emshtml <casadathereza1@gmail.com>
2025-10-27 12:56:39 -0300

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="bg-zinc-950 text-yellow-400 py-5 text-center shadow-lg">
+      <h1 className="text-4xl font-extrabold tracking-wide">🍔 Burger House</h1>
+      <p className="text-zinc-300 mt-1">O sabor que chega até você!</p>
+    </header>
+  );
+}
