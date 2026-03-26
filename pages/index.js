12c99509dc348009f506b9da3ca9c4e6f6b63f41
Emshtml <casadathereza1@gmail.com>
2026-03-26 10:38:56 -0300

    Fix image path for Bolo de Pote

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,7 @@ export default function Home() {
     { nome: "Guaraná 600ml", preco: "R$ 15,00", imagem: "/imagem/guarana-600ml.jpeg" },
     { nome: "Milkshake Chocolate", preco: "R$ 18,00", imagem: "/imagem/milkshake-chocolate.jpeg" },
     { nome: "Milkshake Morango", preco: "R$ 18,00", imagem: "/imagem/milkshake-morango.jpeg" },
-    { nome: "Bolo de Pote", preco: "R$ 22,00", imagem: "/imagem/gemini
-      .png" }
+    { nome: "Bolo de Pote", preco: "R$ 22,00", imagem: "/imagem/bolodepote.png" }
   ]);
 
   const numeroWhatsApp = "5511976794749"; 
